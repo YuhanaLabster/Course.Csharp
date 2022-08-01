b174dfe8166941c170a906f4b5f493cc2a2f0947
Yuhana Atmaja <yuhana@labster.com>
2022-08-01 19:08:40 +0700

    76. Properties Part 1

---
+++ 76. Properties Part 1/Box.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _76._Properties_Part_1
+{
+    internal class Box
+    {
+        // member variable
+        private int length;
+        private int height;
+        private int width;
+        private int volume;
+
+        public void SetLength(int length)
+        {
+            this.length = length;
+        }
+
+        public int GetLength ()
+        {
+            return this.length;
+        }
+
+        public void SetHeight (int height)
+        {
+            this.height = height;
+        }
+        public int GetHeight()
+        {
+            return this.height;
+        }
+
+        public void SetWidth (int width)
+        {
+            this.width = width;
+        }
+
+        public int GetWidth ()
+        {
+            return this.width;
+        }
+
+        public int GetVolume()
+        {
+            return this.length * this.height * this.width;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"lenght is {length} and height is {height}, width is {width} and the volume is {volume = length*width*height}");
+        }
+    }
+}
---
+++ 76. Properties Part 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _76._Properties_Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            box.SetLength(4);
+            box.SetHeight(5);
+            box.SetWidth(10);
+            Console.WriteLine(box.GetVolume());
+            Console.Read();
+        }
+    }
+}
