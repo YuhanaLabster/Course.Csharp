ac73b21af4275a2af67f8ddad25dce23c97eba29
Yuhana Atmaja <yuhana@labster.com>
2022-08-01 20:05:54 +0700

    77. Properties Part 2

---
+++ 77. Properties Part 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _77._Properties_Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box boxes = new Box(10, 12, 3);
+            Console.WriteLine(boxes.Width);
+            Console.WriteLine(boxes.Height);
+            Console.WriteLine(boxes.Width);
+            Console.WriteLine(boxes.Volume);
+        }
+    }
+}
---
+++ 77. Properties Part 2/box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _77._Properties_Part_2
+{
+    internal class Box
+    {
+        public int Length { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public int Volume
+        {
+            get { return this.Length * this.Height * this.Width; }
+        } 
+
+        public Box(int Length, int Height, int Width)
+        {
+            this.Length = Length;
+            this.Height = Height;
+            this.Width = Width;
+        }
+    }
+}
