9e1b63ce5b1cce26392d6ec43a6f83e8856e6b72
Yuhana Atmaja <yuhana@labster.com>
2022-08-01 20:21:36 +0700

    78. Challenge - Properties

---
+++ 78. Challenge - Properties/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _78._Challenge___Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box boxes = new Box(4, 5, 6);
+            Console.WriteLine($"the box Length is {boxes.Length}");
+            Console.WriteLine($"the box Height is {boxes.Height}");
+            Console.WriteLine($"the box Width is {boxes.Width}");
+            Console.WriteLine($"the box Volume is {boxes.Volume}");
+            Console.WriteLine($"the box Front Surface is {boxes.FrontSurface}");
+        }
+    }
+}
---
+++ 78. Challenge - Properties/box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _78._Challenge___Properties
+{
+    internal class Box
+    {
+        public int Length { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public int Volume
+        {
+            get { return this.Length * this.Height * this.Width; }
+        }
+
+        public int FrontSurface 
+        {
+            get { return Length * Width; }
+        }
+
+        public Box(int Length, int Height, int Width)
+        {
+            this.Length = Length;
+            this.Height = Height;
+            this.Width = Width;
+        }
+    }
+}
