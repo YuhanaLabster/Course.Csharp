4b62a3e3255f22d15e52a867e89f9b2833c8b87c
Yuhana Atmaja <yuhana@labster.com>
2022-08-02 19:53:30 +0700

    Coding_Exercise_7_Arrays_for_and_foreach_loops

---
+++ Coding Exercise 7 Arrays for and foreach loops/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Coding_Exercise_7_Arrays_for_and_foreach_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+
+        }
+        public static void GetOdd(int[] Array)
+        {
+            foreach (int i in Array)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine($"{i} is odd number");
+                }
+            }
+        }
+        public static void GetEven(int[] Array)
+        {
+            foreach (int i in Array)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i} is Even number");
+                }
+            }
+        }
+        public static void Run()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            GetOdd(array);
+            GetEven(array);
+
+        }
+
+    }
+}
