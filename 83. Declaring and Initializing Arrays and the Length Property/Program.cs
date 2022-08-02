6dae604a7958afabab8d57d45c861dd8b6a96973
Yuhana Atmaja <yuhana@labster.com>
2022-08-02 18:55:51 +0700

    83. Declaring and Initializing Arrays and the Length Property

---
+++ 83. Declaring and Initializing Arrays and the Length Property/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _83._Declaring_and_Initializing_Arrays_and_the_Length_Property
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 25;
+            grades[2] = 19;
+            grades[3] = 30;
+            grades[4] = 40;
+
+            Console.WriteLine($"element value from index number 3 is {grades[3]}");
+            
+
+        }
+    }
+}
