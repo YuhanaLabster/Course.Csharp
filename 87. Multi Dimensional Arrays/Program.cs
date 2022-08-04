67f6df44dd9085aaacf0959c0f3744b0a606c4f4
Yuhana Atmaja <yuhana@labster.com>
2022-08-04 22:56:06 +0700

    writing amount of money to string

---
+++ 87. Multi Dimensional Arrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _87._Multi_Dimensional_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2D array with singgle comma inside the square bracket "[]"
+            int[,] array2D = new int[,]
+            {
+             // coloumn 0  | coloumn 1 |    index 2
+                {1,             2,             3}, // row 0
+                {4,             5,             6}, // row 1
+                {7,             8,             9}  // row 2  
+            };
+            Console.WriteLine(array2D[2,0]); //to access number 7 we defined call the row then the coloumn
+
+
+            string[,,] array3D = new string[,,]
+            {
+                { 
+                    //  This is array 0, started with 0 at first index [0,0,0]
+                    // coloumn 0  |   coloumn 1
+                    {"Hi"       ,   "hello" },  // row 0 
+                    {"what"     ,   "wow"   },  // row 1
+                    { "you"     ,   "good"  }   // row 2
+                },
+                    //  This is array 1, started with 1 at first index [1,0,0]
+                    // coloumn 0  |   coloumn 1
+                {
+                    {"white"    ,   "blue"  },  // row 0
+                    {"yellow"   ,   "red"   },  // row 1
+                    {"grey"     ,   "black" }   // row 2
+                }
+            };
+            Console.WriteLine(array3D[1,2,0]);
+            /* 
+             * To access "red" : check the array number (1), and then check the row (1), then the coloumn (1) so it would be 1,1,1
+             * To access "wow" : check the array number (0), and then check the row (1), then the coloumn (1) so it would be 0,1,1
+             * To access "grey" : check the array number (1), and then check the row (2), then the coloumn (0) so it would be 0,1,1
+             * 
+            */
+                                   // specifier below separated by comma
+            string[,] array2dTest = new string[3, 2] { {"1","2"}, {"3","4"}, {"5","6"} }; //it means : 3 row, and each row has 2 entries
+
+            string [,] arrayTest =  new string[3, 2] { { "1","2"}, { "3","4"}, { "5","6"} };
+
+            arrayTest[1, 1] = "replaced";
+            Console.WriteLine(arrayTest[0,1]);
+        }
+
+    }
+}
---
+++ self test_Input to string/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace self_test_Input_to_string
+{
+    internal class Program
+    {
+        public static Dictionary<int, string> nominalCurrency = new Dictionary<int, string>();
+        public static Dictionary<int, string> intToString = new Dictionary<int, string>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{nominalCurrency[1]}");
+            Console.WriteLine("input number");
+            string input = Console.ReadLine();
+            long parsed = long.Parse(input);
+            int inputLength = input.Length;
+            }
+        static void Process(int inputLength)
+            {
+            switch (inputLength)
+                {
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        static void addValue()
+        {
+            nominalCurrency.Add(2, "puluh");
+            nominalCurrency.Add(3, "ratus");
+            nominalCurrency.Add(4, "ribu");
+            nominalCurrency.Add(5, "puluh ribu");
+            nominalCurrency.Add(6, "ratus ribu");
+            nominalCurrency.Add(7, "juta");
+            nominalCurrency.Add(8, "puluh juta");
+            nominalCurrency.Add(9, "ratus juta");
+            nominalCurrency.Add(10, "milyar");
+            nominalCurrency.Add(11, "puluh milyar");
+            nominalCurrency.Add(12, "ratus milyar");
+            nominalCurrency.Add(13, "trilyun");
+            intToString.Add(1, "satu");
+            intToString.Add(2, "dua");
+            intToString.Add(3, "tiga");
+            intToString.Add(4, "empat");
+            intToString.Add(5, "lima");
+            intToString.Add(6, "enam");
+            intToString.Add(7, "tujuh");
+            intToString.Add(8, "delapan");
+            intToString.Add(9, "sembilan");
+            intToString.Add(10, "sepuluh");
+        }
+    }
+}
+
+/* ribu         : 1000                  : 4
+ * puluh ribu   : 10 000                : 5
+ * ratus ribu   : 100 000               : 6
+ * juta         : 1 000000              : 7
+ * puluh juta   : 10 000 000            : 8
+ * ratus juta   : 100 000 000           : 9
+ * milyar       : 1 000 000 000         : 10
+ * puluh milyar : 10 000 000 000        : 11
+ * ratus milyar : 100 000 000 000       : 12
+ * trilyun      : 1 000 000 000 000     : 13
+ */
