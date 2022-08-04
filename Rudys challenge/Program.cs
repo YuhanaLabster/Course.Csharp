22afe5f3bbcd91804c14e435a9e57383daf5f671
Yuhana Atmaja <yuhana@labster.com>
2022-08-04 18:24:16 +0700

    create shape based on input

---
+++ Rudys challenge/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rudys_challenge
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //Triangle();
+            Square();
+        }
+        static void Square()
+        {
+            Console.WriteLine("enter single character you want to show");
+            string Characters = Console.ReadLine();
+            char ParsedCharacter = Characters.ToCharArray()[0];
+            Console.WriteLine("How many times will be repeated?");
+            string input = Console.ReadLine();
+            int ParsedInput = int.Parse(input);
+            Console.WriteLine("Here's the square");
+            for (int i = ParsedInput; i > 0; i--)
+            {
+                Console.WriteLine(new string(ParsedCharacter, ParsedInput));
+            }
+        }
+
+        static void PointUpTriangle() //finished
+        {
+            Console.WriteLine("enter single character you want to show");
+            string Characters = Console.ReadLine();
+            char ParsedCharacter = Characters.ToCharArray()[0];
+            Console.WriteLine("How many times will be repeated?");
+            string input = Console.ReadLine();
+            int ParsedInput = int.Parse(input);
+            Console.WriteLine("Here's the triangle");
+            for (int i = 0; i < ParsedInput; i++)
+            {
+                Console.Write(ParsedCharacter);
+                Console.WriteLine(new string(ParsedCharacter, i));
+            }
+        }
+        static void PointDownTriangle() //finished
+        {
+            Console.WriteLine("enter single character you want to show");
+            string Characters = Console.ReadLine();
+            char ParsedCharacter = Characters.ToCharArray()[0];
+            Console.WriteLine("How many times will be repeated?");
+            string input = Console.ReadLine();
+            int ParsedInput = int.Parse(input);
+            Console.WriteLine("Here's the triangle");
+            for (int i = ParsedInput; i > 0; i--)
+            {
+                Console.WriteLine(new string(ParsedCharacter, i));
+            }
+        }
+    }
+}
