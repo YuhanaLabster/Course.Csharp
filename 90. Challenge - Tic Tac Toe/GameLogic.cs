ef16ec59f6fa7507a4f8d0987faa248a280f4de4
Yuhana Atmaja <yuhana@labster.com>
2022-08-12 22:10:05 +0700

    Add logic to check the winner based on row, column, and diagonal

--- 90. Challenge - Tic Tac Toe/GameLogic.cs
+++ 90. Challenge - Tic Tac Toe/GameLogic.cs
@@ -31,6 +31,7 @@ namespace _90._Challenge___Tic_Tac_Toe
                         Input.CharacterReminder();
                         Display.DrawScreen();
                         Input.ArrayUpdater(Input.PlayerInput(i), Variables.Player[i]);
+                        LineChecker(i);
                         Display.RefreshScreen();
                     }
             }
@@ -59,6 +60,74 @@ namespace _90._Challenge___Tic_Tac_Toe
                 isContinue = false;
             }
         }
+
+        public static void LineChecker(int PlayerKey)
+        {
+            FirstRow(PlayerKey);
+            SecondRow(PlayerKey);
+            ThirdRow(PlayerKey);
+            FirstColumn(PlayerKey);
+            SecondColumn(PlayerKey);
+            ThirdColumn(PlayerKey);
+            Diagonal(PlayerKey);
+        }
+        public static void FirstRow(int PlayerKey)
+        {
+            //[0, 0], [0, 1],[0, 2]
+            if (Variables.Player[PlayerKey].Equals(Variables.Board[0, 0]) && Variables.Player[PlayerKey].Equals(Variables.Board[0, 1]) && Variables.Player[PlayerKey].Equals(Variables.Board[0, 2]))
+            {
+                Debug.WriteLine($"Player {PlayerKey} is the winner!");
+            }
+        }
+        public static void SecondRow(int PlayerKey)
+        {
+            //[1, 0], [1, 1],[1, 2]
+            if (Variables.Player[PlayerKey].Equals(Variables.Board[1, 0]) && Variables.Player[PlayerKey].Equals(Variables.Board[1, 1]) && Variables.Player[PlayerKey].Equals(Variables.Board[1, 2]))
+            {
+                Debug.WriteLine($"Player {PlayerKey} is the winner!");
+            }
+        }
+        public static void ThirdRow(int PlayerKey)
+        {
+            //[2, 0], [2, 1],[2, 2]
+            if (Variables.Player[PlayerKey].Equals(Variables.Board[2, 0]) && Variables.Player[PlayerKey].Equals(Variables.Board[2, 1]) && Variables.Player[PlayerKey].Equals(Variables.Board[2, 2]))
+            {
+                Debug.WriteLine($"Player {PlayerKey} is the winner!");
+            }
+        }
+        public static void FirstColumn(int PlayerKey)
+        {
+            //[0, 0], [1, 0],[2, 0]
+            if (Variables.Player[PlayerKey].Equals(Variables.Board[0, 0]) && Variables.Player[PlayerKey].Equals(Variables.Board[1, 0]) && Variables.Player[PlayerKey].Equals(Variables.Board[2, 0]))
+            {
+                Debug.WriteLine($"Player {PlayerKey} is the winner!");
+            }
+        }
+        public static void SecondColumn(int PlayerKey)
+        {
+            //[0, 1], [1, 1],[2, 1]
+            if (Variables.Player[PlayerKey].Equals(Variables.Board[0, 1]) && Variables.Player[PlayerKey].Equals(Variables.Board[1, 1]) && Variables.Player[PlayerKey].Equals(Variables.Board[2, 1]))
+            {
+                Debug.WriteLine($"Player {PlayerKey} is the winner!");
+            }
+        }
+        public static void ThirdColumn(int PlayerKey)
+        {
+            //[0, 2], [1, 2],[2, 2]
+            if (Variables.Player[PlayerKey].Equals(Variables.Board[0, 2]) && Variables.Player[PlayerKey].Equals(Variables.Board[1, 2]) && Variables.Player[PlayerKey].Equals(Variables.Board[2, 2]))
+            {
+                Debug.WriteLine($"Player {PlayerKey} is the winner!");
+            }
+        }
+        public static void Diagonal(int PlayerKey)
+        {
+            //[0, 0], [1, 1],[2, 2]
+            if (Variables.Player[PlayerKey].Equals(Variables.Board[0, 0]) && Variables.Player[PlayerKey].Equals(Variables.Board[1, 1]) && Variables.Player[PlayerKey].Equals(Variables.Board[2, 2]))
+            {
+                Debug.WriteLine($"Player {PlayerKey} is the winner!");
+            }
+        }
+
 
     }
 
