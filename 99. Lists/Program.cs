ed3b90f6efdaf162afe7f7a321b9c1ac02490e74
Yuhana Atmaja <yuhana@labster.com>
2022-08-16 13:46:06 +0700

    99. Lists

---
+++ 99. Lists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _99._Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = new List<int>();
+
+            /* 
+            List single type of collection and using using System.Collections.Generic
+            List of type int without value : var numbers = new List<int>();
+            List of type int with value : var number = new List<int>{1,5,35,100};
+
+            add number : 
+            numbers.Add(25);
+
+            remove number : 
+            numbers.Remove(25); //remove the exact number (25) from the list
+            numbers.RemoveAt(0); //remove number based on index 0.
+
+            clear number : 
+            numbers.Clear(); // this will remove all elements inside that list
+
+            Loop : 
+            foreach (int element in numbers)
+            {
+                Console.WriteLine(element);
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(i);
+            }
+                 
+             */
+
+
+        }
+    }
+}
