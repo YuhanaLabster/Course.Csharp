bfe1f468d1f87740554438e198a12aa61c373d2a
Yuhana Atmaja <yuhana@labster.com>
2022-08-16 14:15:07 +0700

    Coding Exercise 9 Lists

---
+++ Coding Exercise 9 Lists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coding_Exercise_9_Lists
+{
+    internal class Program
+    {
+        public static List<int> MyList = new List<int>();
+
+        static void Main(string[] args)
+        {
+            for (int i = 100; i <= 170; i++)
+            {
+                MyList.Add(i++);
+            }
+
+            foreach (int i in MyList)
+            {
+                Console.Write(i+" ");
+            }   
+        }
+    }
+}
