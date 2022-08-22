d8c75565f6aee726de8843f00dd598d64a37fc6e
Yuhana Atmaja <yuhana@labster.com>
2022-08-22 16:33:22 +0700

    100. Hashtables

---
+++ 100. Hashtables/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace _100._Hashtables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // hastables almost similar to dictionaries
+            // has key and value
+
+            Hashtable studentstable = new Hashtable();
+
+            Student stud1 = new Student(1, "yanto", 98);
+            Student stud2 = new Student(2, "bambang", 91);
+            Student stud3 = new Student(3, "eko", 45);
+            Student stud4 = new Student(4, "paryadi", 89);
+            Student stud5 = new Student(5, "sutarno", 76);
+
+            studentstable.Add(stud1.Id, stud1);
+            studentstable.Add(stud2.Id, stud2);
+            studentstable.Add(stud3.Id, stud3);
+            studentstable.Add(stud4.Id, stud4);
+            studentstable.Add(stud5.Id, stud5);
+
+            Student storedStudent1 = (Student)studentstable[stud1.Id];
+
+            //get individual item from hastables
+            foreach (DictionaryEntry entry in studentstable)
+            {
+                Student temp = (Student)entry.Value;
+                Console.WriteLine($"Student ID {temp.Id}");
+                Console.WriteLine($"Student Name {temp.name}");
+                Console.WriteLine($"Student GPA {temp.GPA}");
+            }
+            //get individual item from known id
+            Console.WriteLine($"Student ID : {storedStudent1.Id}, Name : {storedStudent1.name}, GPA : {storedStudent1.GPA}");
+
+            //we can access the value directly by calling its method
+            foreach (Student value in studentstable.Values)
+            {
+                Console.WriteLine($"Student ID {value.Id}");
+                Console.WriteLine($"Student Name {value.name}");
+                Console.WriteLine($"Student GPA {value.GPA}");
+            }
+        }
+    }
+}
---
+++ 100. Hashtables/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100._Hashtables
+{
+    internal class Student
+    {
+        public int Id { get; set; } //property called Id
+        public string name { get; set; } //property called name
+        public float GPA { get; set; } //property called GPA
+
+        //simple constractor
+        public Student (int Id, string name, float GPA)
+        {
+            this.Id = Id;
+            this.name = name;
+            this. GPA = GPA;
+        }
+    }
+}
