25b56148afe6faff361de2b60df81b5e90ad19b5
Yuhana Atmaja <yuhana@labster.com>
2022-08-22 17:16:21 +0700

    101. Hashtables - Challenge

---
+++ 101. Hashtables - Challenge/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace _101._Hashtables___Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* write program that will iterate through each element of the students array and insert them into hastables
+             * If a student with the same ID already exist in the hashtables skip it and display the following error :
+             * "sorry, A student with the same ID already Exists"
+             * Hint : Use method ContainsKey() to check wether a student with the same ID already exist
+             */
+            Hashtable table = new Hashtable();
+
+
+            Student[] students = new Student[5];
+
+            students[0] = new Student(1, "Denis", 88);
+            students[1] = new Student(2, "Olaf", 97);
+            students[2] = new Student(6, "Ragner", 65);
+            students[3] = new Student(1, "Luise", 73);
+            students[4] = new Student(4, "Levi", 58);
+            
+            foreach (Student s in students)
+            {
+                if (!table.ContainsKey(s.Id))
+                {
+                    table.Add(s.Id, s);
+                    Console.WriteLine($"{s.Id} was added");
+                }
+                else
+                {
+                    Console.WriteLine($"sorry, A student with the same ID {s.Id} already Exists");
+                }
+            }
+        }
+    }
+}
---
+++ 101. Hashtables - Challenge/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _101._Hashtables___Challenge
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Grade { get; set; }
+
+        public Student(int Id, string Name, float Grade)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Grade = Grade;
+        }
+
+    }
+
+
+}
