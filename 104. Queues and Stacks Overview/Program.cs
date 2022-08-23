aacb43ffdbe054fa42abb113d4cfd3a90f320e9a
Yuhana Atmaja <yuhana@labster.com>
2022-08-23 12:07:46 +0700

    104. Queues and Stacks Overview

---
+++ 104. Queues and Stacks Overview/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _104._Queues_and_Stacks_Overview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* 
+            
+            STACKS : 
+                * Data can be added/removed from top but cannot be added or removed from the middle or the bottom part.
+                * Can't access element in the middle
+                * First come last Out or last in first out (LIFO)
+
+               STACKS Example :  
+                * Reversing Data
+                * Web browser back button, user able to go back to previous page
+                * Undo/Redo buttons.
+             
+               STACKS Operations : 
+                * Push (Object) to add data to the stack
+                * Pop (object) to remove data from the stack
+                * Peek (object) to return object data without removing it.
+                
+            QUEUES : 
+                * Data can be added from rear (back) and removed from the front.
+                * Can't access element in the middle
+                * First in first out (FIFO)
+                
+                QUEUES Examples : 
+                * OS IO request, mouse movement 
+                * Managing web request in a server
+                * Queuing input in video games
+                
+                QUEUES Operations : 
+                * Enqueue (object) add data to the queue, data will be added to the back of the queue.
+                * Dequeue (object) remove the data from the front of the queue.
+                * Peek () return the object in front of the queue
+             
+             */
+        }
+    }
+}
