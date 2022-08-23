5cbd68bef784aa1f0c8ae6ac2d2e49d4acf44a92
Yuhana Atmaja <yuhana@labster.com>
2022-08-23 11:47:52 +0700

    Coding Exercise 10 Dictionary practice

---
+++ Coding Exercise 10 Dictionary practice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coding_Exercise_10_Dictionary_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Speller(1));
+        }
+        public static string Speller (int i)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>()
+            {
+                {0,"zero"},
+                {1,"one"},
+                {2,"two"},
+                {3,"three"},
+                {4,"four"},
+                {5,"Five"},
+            };
+
+            if (dic.ContainsKey(i))
+            {
+                return dic[i];
+            }
+            else
+                return "nope";
+        }
+
+    }
+}
