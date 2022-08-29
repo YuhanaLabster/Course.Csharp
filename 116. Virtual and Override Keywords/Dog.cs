0441e8344ff238a925e62d2c0872edd1d3adf2c1
Yuhana Atmaja <yuhana@labster.com>
2022-08-29 22:10:28 +0700

    116. Virtual and Override Keywords

---
+++ 116. Virtual and Override Keywords/Dog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _116._Virtual_and_Override_Keywords
+{
+    internal class Dog :Animal
+    {
+        public bool IsHappy { get; set; }
+
+        public Dog(string Name, int Age) :base(Name, Age)
+        {
+            IsHappy = true;
+        }
+
+        public override void Eat()
+        {
+            base.Eat();
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("wuuuf wuuf");
+        }
+
+        public void Play()
+        {
+            if (IsHappy)
+            {
+                base.Play();
+            }
+        }
+    }
+}
---
+++ 116. Virtual and Override Keywords/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _116._Virtual_and_Override_Keywords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Anjing = new Dog("helly", 10);
+            Console.WriteLine($"{Anjing.Name} is {Anjing.Age} years old");
+            Anjing.MakeSound();
+            Anjing.Play();
+            Anjing.Eat();
+
+
+            /* STORY 
+                * We have Animal that act as parent class
+                * But we want to modify function inside Animal class from child class (Dog)
+              
+                HOW : 
+                * PARENT CLASS (Animal)
+                    * add "virtual" into the function name
+                * CHILD CLASS (Dog)
+                    * add "override" into the function name
+                    * If we want to have the same function as Parent, we can add "Base.Function", example "base.Eat()"
+             */
+
+        }
+    }
+}
