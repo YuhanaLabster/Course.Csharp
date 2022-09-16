090afd2daa34d9324eb40c3bd1b5c17bfabdeb6b
Yuhana Atmaja <yuhana@labster.com>
2022-09-16 12:35:34 +0700

    Interfaces Intro

---
+++ Interfaces Intro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Interfaces_Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket ticket_1 = new Ticket(10);
+            Ticket ticket_2 = new Ticket(10);
+            Console.WriteLine(ticket_1.Equals(ticket_2));
+
+        }
+    }
+}
---
+++ Interfaces Intro/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_Intro
+{
+    internal class Ticket : IEquatable<Ticket>
+    {
+        //simple property
+        public int DurationInHours { get; set; }
+
+        //simple constructors
+        public Ticket(int durationInHours)
+        {
+            DurationInHours = durationInHours;
+        }
+
+        public bool Equals(Ticket other)
+        {
+            return this.DurationInHours == other.DurationInHours;
+        }
+    }
+}
