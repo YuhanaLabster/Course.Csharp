fe6f7327875277ad5f40618190022057516c4a04
Yuhana Atmaja <yuhana@labster.com>
2023-02-01 15:03:58 +0700

    150._Garbage_Collector

---
+++ 150. Garbage Collector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _150._Garbage_Collector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
