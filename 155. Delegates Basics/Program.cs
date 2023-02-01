0f74a6591de9bad2bd11b095ddd578e22b901a67
Yuhana Atmaja <yuhana@labster.com>
2023-02-01 18:09:59 +0700

    155. Delegates Basics

---
+++ 155. Delegates Basics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _155._Delegates_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Aiden", "sif","walter","Anatoli"};
+            
+            
+            Console.WriteLine("----before-----");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.RemoveAll(Filter);
+
+            Console.WriteLine("----after-----");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
+        }
+
+        static bool Filter(string input)
+        {
+            return input.Contains("i");
+        }
+    }
+}
