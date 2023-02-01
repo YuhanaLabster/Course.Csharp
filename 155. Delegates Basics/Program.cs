6597fc4a75de4eec47f6f07861064e3bb70574dc
Yuhana Atmaja <yuhana@labster.com>
2023-02-01 18:10:58 +0700

    forgot to save file

--- 155. Delegates Basics/Program.cs
+++ 155. Delegates Basics/Program.cs
@@ -16,7 +16,7 @@ namespace _155._Delegates_Basics
                 Console.WriteLine(name);
             }
 
-            names.RemoveAll(Filter);
+            names.RemoveAll(Filter); // in this case we don't have to add (string input) while calling Filter.
 
             Console.WriteLine("----after-----");
             foreach (string name in names)
---
+++ 156. Creating your own Delegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _156._Creating_your_own_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
