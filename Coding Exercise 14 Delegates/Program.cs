e9ce9a2f4c165ff6a710dca4fb79c884e33ccbaa
Yuhana Atmaja <yuhana@labster.com>
2023-03-02 18:26:27 +0700

    Coding_Exercise_14_Delegates

---
+++ Coding Exercise 14 Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Coding_Exercise_14_Delegates
+{
+    internal class Program
+    {
+        public delegate float OperationDelegate(float x, float y);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divide(5, 3));
+        }
+
+        public static float Subtract(float x, float y) { return x - y; }
+        public static float Add(float x, float y){return x + y;}
+        public static float Multiply(float x, float y) { return x * y; }
+        public static float Divide(float x, float y) { return x / y; }
+
+        public static float ApplyOperation(float x, float y, OperationDelegate operation)
+        {
+            float result;
+            result= operation(x, y);
+            return result;
+        }
+    }
+}
