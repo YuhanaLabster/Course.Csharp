03647ca22d4f466a3d5dd353732832f0cf8222e8
Yuhana Atmaja <yuhana@labster.com>
2023-03-02 19:34:40 +0700

    update

--- 159. Lambda Expressions/Program.cs
+++ 159. Lambda Expressions/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using static _156._Creating_your_own_Delegates.Program;
 
-namespace _156._Creating_your_own_Delegates
+namespace _159._Lambda_Expressions
 {
     internal class Program
     {
@@ -12,27 +11,49 @@ namespace _156._Creating_your_own_Delegates
         static void Main(string[] args)
         {
             // create 4 person objects
-            Person p1 = new Person() { Name = "Aiden",Age = 41};
+            Person p1 = new Person() { Name = "Aiden", Age = 41 };
             Person p2 = new Person() { Name = "Sif", Age = 69 };
             Person p3 = new Person() { Name = "Walter", Age = 12 };
             Person p4 = new Person() { Name = "Anatoli", Age = 25 };
 
             // add the object to a list called people
-            List<Person> people = new List<Person>() { p1,p2,p3,p4};
+            List<Person> people = new List<Person>() { p1, p2, p3, p4 };
 
             DisplayPeople("Kids", people, IsMinor);
             DisplayPeople("Adult", people, IsAdult);
             DisplayPeople("Senior", people, IsSenior);
 
+            // here we created a new variable called filter of type FilterDelegate
+            // then we assigned an anonymous method to it instead of an already defined method
+
+            FilterDelegate filter = delegate (Person p)
+            {
+                return p.Age >= 20 && p.Age <= 30;
+            };
+
+            DisplayPeople("p.Age >= 20 && p.Age <=30;", people, filter);
+            DisplayPeople("ALL", people, delegate (Person p) { return true; });
+
+            string searchKeyword = "A";
+            DisplayPeople("Age > 20 with search keyword : " + searchKeyword, people,p => 
+            {
+                if (p.Name.Contains(searchKeyword) && p.Age >20)
+                {
+                    return true;
+                }
+                return false;
+            });
+
+            DisplayPeople("Exactly 25:", people, p => p.Age == 25);
         }
         //a method to display the list of people that passes the filter condition
         // (returns true)
         // this method will take a title to be displayed, the list of people,
         //land a filter delegate
-        static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+        public static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
         {
             Console.WriteLine(title);
-            foreach(Person p in people) 
+            foreach (Person p in people)
             {
                 // check if the persons passes the filter
                 if (filter(p))
@@ -42,6 +63,8 @@ namespace _156._Creating_your_own_Delegates
                 }
             }
         }
+        // statement lambda
+        // a search keyword to filter the names 
 
         // filters
         static bool IsMinor(Person p)
@@ -58,5 +81,7 @@ namespace _156._Creating_your_own_Delegates
         {
             return p.Age >= 65;
         }
+
+        
     }
 }
--- Coding Exercise 14 Delegates/Program.cs
+++ Coding Exercise 14 Delegates/Program.cs
@@ -4,23 +4,23 @@ namespace Coding_Exercise_14_Delegates
 {
     internal class Program
     {
-        public delegate float OperationDelegate(float x, float y);
+            public delegate float OperationDelegate(float x, float y);
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Divide(5, 3));
-        }
+            static void Main(string[] args)
+            {
+                Console.WriteLine(Divide(5, 3));
+            }
 
-        public static float Subtract(float x, float y) { return x - y; }
-        public static float Add(float x, float y){return x + y;}
-        public static float Multiply(float x, float y) { return x * y; }
-        public static float Divide(float x, float y) { return x / y; }
+            public static float Subtract(float x, float y) { return x - y; }
+            public static float Add(float x, float y){return x + y;}
+            public static float Multiply(float x, float y) { return x * y; }
+            public static float Divide(float x, float y) { return x / y; }
 
-        public static float ApplyOperation(float x, float y, OperationDelegate operation)
-        {
-            float result;
-            result= operation(x, y);
-            return result;
-        }
+            public static float ApplyOperation(float x, float y, OperationDelegate operation)
+            {
+                float result;
+                result= operation(x, y);
+                return result;
+            }
     }
 }
