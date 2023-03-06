ad94292eeb3ea8401b3efe0466e20390144a657e
Yuhana Atmaja <yuhana@labster.com>
2023-03-06 16:49:21 +0700

    update

--- 161. Events and Multicast Delegates/Program.cs
+++ 161. Events and Multicast Delegates/Program.cs
@@ -48,8 +48,23 @@ namespace _161._Events_and_Multicast_Delegates
             GameEventManager.TriggerGameOver();
             // ----------------------------------------------- END : WITH multicast -----------------------------------------------
 
+            // --------------- introducing EVENT --------------- 
+            // why we need an event? because the common issue is we're sometime assign the method BUT NOT SUBSCRIBING TO IT
+            // EXAMPLE : "+= StartGame;" is CORRECT as its subscribing, but `= StartGame`; is wrong, its assining not subscribing!
+            // the IDE won't give an error when we do that, that's why we need an "event" to secure it
+            // by this way we wont make mistake to subscribe it
+            // how to do that? we will add 'event' before in GameEvent method
+            // from public static GameEvent OnGameStart, OnGameOver;
+            // to public event static GameEvent OnGameStart, OnGameOver;
+            // by this way when we do `= StartGame` IDE will throw an error : 
+            // "The event 'GameEventManager.OnGameStart' can only appear on the left hand side of += or -=
+            // (except when used from within the type 'GameEventManager')
 
-
+            // --------------------------- KEY POINTS ---------------------------
+            // EVENT are forced to behave like a list : 
+            // "+=" to subscribe or "-=" to unsubscribe are allowed but "=" not allowed
+            // Delegates allow a direct assignment : myDelegate = myMethod
+            // EVENT can not be triggered from outside the class that defines them
         }
     }
 }
